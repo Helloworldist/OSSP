a6a553add394d3a4ec6718199194c616eef795bf
Ssol <nsm1027@naver.com>
2018-01-10 00:56:01 +0900

    Beautifulsoup test3

--- bigdata/test.py
+++ bigdata/test.py
@@ -8,17 +8,18 @@ def getTitle(url):
     except HTTPError as e:
         return  None
     try:
-        bsObj=BeautifulSoup(html.read, "html.parser")
+        bsObj = BeautifulSoup(html.read(), "html.parser")
         title = bsObj.body.h1
     except AttributeError as e:
         return None
     return title
 
-title=getTitle("http://www.w3.org/1999/xhtml")
+title = getTitle("https://ko.wikipedia.org/wiki/%EC%9C%A0%EB%A1%9C%ED%8C%8C_(%EC%9C%84%EC%84%B1)")
 if title == None:
     print("Title couldn't found")
 else:
     print(title)
+
 # html = urlopen("http://www.pythonscraping.com/pages/page1.html")
 # bsObj = BeautifulSoup(html.read(), "html.parser")
 # print(bsObj.div)
