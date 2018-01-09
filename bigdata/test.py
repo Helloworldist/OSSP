eb1bbe2d4e5bfadf0ce1e7b76933932ae8eec3c4
Ssol <nsm1027@naver.com>
2018-01-10 00:49:26 +0900

    Beautifulsoup test2

--- bigdata/test.py
+++ bigdata/test.py
@@ -1,6 +1,24 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), "html.parser")
-print(bsObj.div)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return  None
+    try:
+        bsObj=BeautifulSoup(html.read, "html.parser")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title=getTitle("http://www.w3.org/1999/xhtml")
+if title == None:
+    print("Title couldn't found")
+else:
+    print(title)
+# html = urlopen("http://www.pythonscraping.com/pages/page1.html")
+# bsObj = BeautifulSoup(html.read(), "html.parser")
+# print(bsObj.div)
