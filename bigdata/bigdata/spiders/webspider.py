ef8c7d55a0c9edcab961ef5510a4da9391d73efd
Ssol <nsm1027@naver.com>
2018-01-13 16:44:11 +0900

    scraping test - job

--- bigdata/bigdata/spiders/webspider.py
+++ bigdata/bigdata/spiders/webspider.py
@@ -1,9 +1,8 @@
 import pypiwin32_system32
 import scrapy
-import sys
+import re
 from scrapy.spider import Spider
 from scrapy.selector import Selector
-#from APT2U.items import Apt2UItem
 from scrapy.http import Request
 from scrapy.selector import HtmlXPathSelector
 #sys.setdefaultencoding('utf-8')
@@ -14,27 +13,50 @@ from scrapy.item import Item, Field
 class WebItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    area = scrapy.Field()    # 영화
-    content = scrapy.Field()       # 평
-    company = scrapy.Field()    # 평점
-    pay = scrapy.Field()  # 작성자
-    time = scrapy.Field()  # 날짜
+    company = scrapy.Field()
+    subject = scrapy.Field()
+    category = scrapy.Field()
+    condition = scrapy.Field()
+    date = scrapy.Field()
 
 
 class WebSpider(scrapy.Spider):
-    name="alba"
-    allowed_domains = ["www.albamon.com"]   # 크롤링할 최상위 도메인
-    start_urls = ["http://www.albamon.com/list/gi/mon_area.asp?scd=E000&gcd=E000"] # 실제 크롤링 대상 주소
+    name = "job"
+    allowed_domains = ["www.incruit.com"]   # 크롤링할 최상위 도메인
+    start_urls = ["http://job.incruit.com/jobdb_list/searchjob.asp?ct=3&ty=2&cd=15"] # 실제 크롤링 대상 주소
+
+    #def start_requests(self):
+     #   for i in range(1, 5, 1):
+      #      yield scrapy.Request("http://movie.naver.com/movie/point/af/list.nhn?page=1" % i, self.parse)
 
     def parse(self, response):
-        hxs = HtmlXPathSelector(response)    # 지정된 주소에서 전체 소스코드를 가져옴
-        sites = hxs.select('//tbody/td[@id="dev_preview__53572101"]')
-        items = []   # 데이터를 item별로 구별해서 담을 리스트
-
-        for site in sites:
+        for select in response.xpath('//tbody/tr'):
             item = WebItem()
-            item['area']= site.xpath('//*[@id="dev_preview__53572101"]/td[1]/text()').extract()[0]
-            item['pay']=site.xpath('//*[@id="dev_preview__53572101"]/td[4]/text()').extract()
-            items.append(item)
-            return items
+            item['company'] = select.xpath('th[@scope="row"]/div[@class="companys check_companys"]/div[@class="check_list_r"]/span[@class="links"]/a/text()').extract_first()
+            item['subject'] = select.xpath('td[@valign="top"]/div[@class="subjects"]/span[@class="accent"]/a/text()').extract_first()
+            item['category'] = select.xpath('td[@valign="top"]/div[@class="subjects"]/p[@class="details_txts firstChild"]/em/text()').extract_first()
+            item['condition'] = select.xpath('td[@valign="top"]/div[@class="subjects termArea"]/p[@class="details_txts firstChild"]/em/text()').extract_first()
+            item['date'] = select.xpath('td[@valign="top"]/div[@class="ddays"]/p/text()').extract_first()
+
+            print( '='*50)
+            print('기업명 : ' + item['company'])
+            print('채용제목 : ' + item['subject'])
+            print('분야 : ' + item['category'])
+            print('근무조건 : ' + item['condition'])
+            print('마감일 : ' + item['date'])
+            print('='*50)
+            yield item
+
+
+
+        #hxs = HtmlXPathSelector(response)    # 지정된 주소에서 전체 소스코드를 가져옴
+        #sites = hxs.select('//tbody/td[@id="dev_preview__53572101"]')
+        #items = []   # 데이터를 item별로 구별해서 담을 리스트
+
+        #for site in sites:
+         #   item = WebItem()
+          #  item['area']= site.xpath('//*[@id="dev_preview__53572101"]/td[1]/text()').extract()[0]
+           # item['pay']=site.xpath('//*[@id="dev_preview__53572101"]/td[4]/text()').extract()
+            #items.append(item)
+            #return items
 
