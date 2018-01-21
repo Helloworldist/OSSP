11d9617a0fa0fa713e4100596aef6fe7879cc8f4
Ssol <nsm1027@naver.com>
2018-01-21 21:59:01 +0900

    data analysis - wordcloud

--- bigdata/bigdata/analysis.py
+++ bigdata/bigdata/analysis.py
@@ -1,38 +1,50 @@
-import rpy2.robjects.packages as rpackages  # rpy2의 패키지 모듈 임포트
-from rpy2.robjects.vectors import StrVector # R 스트링 벡터
-import rpy2.robjects as robj
-from rpy2.robjects import r
-import os
-
-utils=rpackages.importr('utils')    # R 기본 utils 패키지 임포트
-utils.chooseCRANmirror(ind=1)   # 리스트에서 첫번째 미러 선택
-
-packname=('wordcloud', 'ggplot2')   # 다운로드 할 패키지
-
-# utils.install_packages(StrVector(packname))     # R 유틸리티 패키지로 packname의 패키지 설치
-
 # 구현 방향
 # 1. 파일 선택
 # 2. 분석, 시각화
 
-#base_dir = 'E:\webcrawl\webcrawl'
-#input_file = 'news_test'
-#output_file = 'test.png'
-#input_mat = utils.read_csv(os.path.join(base_dir, input_file +'.csv')
-#                           , fileEncoding="euc-kr")
-#print(input_mat)
+from rpy2.robjects import r
+import os
+import rpy2.robjects.packages as rpackages
+import rpy2.robjects as robj
+# R 기본 패키지
+utils=rpackages.importr('utils')
 
-robj.r('setwd("E:/")')
-robj.r('data<-read.csv("news2.csv", header=TRUE, fileEncoding="euc-kr", sep=",")')
-#print(robj.r('data'))
+# 경로 지정 및 관련 라이브러리 호출
+robj.r('setwd("E:/webcrawl/webcrawl")')
+robj.r('Sys.setenv(JAVA_HOME="C:/Program Files (x86)/Java/jre1.8.0_161")')
+r.library('rJava')
+r.library('wordcloud')
+r.library('RColorBrewer')
+r.library('KoNLP')
+#robj.r('useSejongDic()')
 
-a=robj.r('hi<-c(4214,312,234,551)')
-b=robj.r('no<-c("Cat", "Dog", "Mouse", "Bear")')
-robj.r('library(wordcloud)')
-robj.r('library(RColorBrewer)')
+# 데이터 분석 - 파일 불러오기, 명사 추출, 및 기타 필터링
+print("데이터 분석 중입니다...[파일 불러오기]")
+robj.r('data1<-readLines("news_latest.csv", encoding = "utf-8")')
+robj.r('data2<-sapply(data1, extractNoun, USE.NAMES = F)')
+robj.r('data3<-unlist(data2)')
+robj.r('data3<-Filter(function(x){nchar(x)>=2}, data3)')
+print("데이터 분석 중입니다...[명사 추출 및 필터링]")
+robj.r('data3<-gsub("\\\\d+","",data3)')
+robj.r('data3<-gsub("\\\\(","",data3)')
+robj.r('data3<-gsub("\\\\)","",data3)')
+robj.r('data3<-gsub("\\\\[","",data3)')
+robj.r('data3<-gsub("\\\\]","",data3)')
+robj.r('data3<-gsub("오전","",data3)')
+robj.r('data3<-gsub("오후","",data3)')
+robj.r('data3<-gsub("사진","",data3)')
+robj.r('data3<-gsub("kr","",data3)')
 
+# 데이터 분석 - 공백 제거 및 단어 빈도 확인
+print("데이터 분석 중입니다...[공백 제거 및 빈도 카운트]")
+robj.r('write(unlist(data3), "tmp_eData.txt")')
+robj.r('data4<-read.table("tmp_eData.txt")')
+robj.r('wordcount<-table(data4)')
+robj.r('palette<-brewer.pal(9,"Set1")')
+
+# 데이터 시각화 - 워드클라우드 생성 및 저장
+print("데이터 시각화 중입니다...[워드클라우딩]")
 robj.r('png(filename="E:/wc.png", bg="white")')
-robj.r('wordcloud(no, hi, color="green3")')
+robj.r('wordcloud(names(wordcount), freq=wordcount, scale=c(6,2), rot.per = 0.1, min.freq = 1, random.order = F, random.color = T, colors = palette)')
 robj.r('dev.off()')
-print(a, b)
-
+print("데이터 분석 및 시각화 완료")
